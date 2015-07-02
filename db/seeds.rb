43a93068002f02d9e5d25241ccc267ff7bfdf0c1
Logan Sears <lsears322@gmail.com>
2015-07-01 20:51:00 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ruby_topic       = FactoryGirl.create(:topic, name: 'Ruby')
+javascript_topic = FactoryGirl.create(:topic, name: 'Javascript')
+painting_topic   = FactoryGirl.create(:topic, name: 'Painting')
+climbing_topic   = FactoryGirl.create(:topic, name: 'Rock Climbing')
+
+ruby_guides = [
+    FactoryGirl.create(:guide, title: 'Setting up your dev environment'),
+    FactoryGirl.create(:guide, title: 'Basic data types'),
+    FactoryGirl.create(:guide, title: 'Using classes to create objects'),
+    FactoryGirl.create(:guide, title: 'Using inheritence to DRY up your code')
+]
+
+javascript_guides = [
+    FactoryGirl.create(:guide, title: 'The basics of Prototypes'),
+    FactoryGirl.create(:guide, title: 'Closures and headaches'),
+    FactoryGirl.create(:guide, title: 'How hoisting affects your code')
+]
+
+painting_guides = [
+    FactoryGirl.create(:guide, title: 'The basics of color theory'),
+    FactoryGirl.create(:guide, title: 'Framing for dramatic effect')
+]
+
+climbing_guides = [
+    FactoryGirl.create(:guide, title: 'Safety while belaying'),
+    FactoryGirl.create(:guide, title: 'Choosing the right shoes'),
+    FactoryGirl.create(:guide, title: 'Lead climbing explained')
+]
+
+ruby_guides.each       { |guide| FactoryGirl.create(:tag, guide: guide, topic: ruby_topic) }
+javascript_guides.each { |guide| FactoryGirl.create(:tag, guide: guide, topic: javascript_topic) }
+painting_guides.each   { |guide| FactoryGirl.create(:tag, guide: guide, topic: painting_topic) }
+climbing_guides.each   { |guide| FactoryGirl.create(:tag, guide: guide, topic: climbing_topic) }
