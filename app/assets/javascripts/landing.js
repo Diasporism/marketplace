30ef3398250ba717ed6ba3ab738eb526a93ce64f
Logan Sears <lsears322@gmail.com>
2015-07-05 13:22:59 -0600

    Make nav dropdown dynamic based on scroll position

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,8 +1,34 @@
 $(function() {
-    function OpenNavigator() {
+    function openNavigator() {
         $('#nav-dropdown').toggle();
     }
 
-    $('#nav-button').click(OpenNavigator);
-    $('#nav-section').click(OpenNavigator);
+    function updateNavigator(text) {
+        $('#nav-section').html(text);
+        $('#nav-dropdown li').css("font-weight","normal");
+        $('#nav-dropdown li').filter(function() {
+            return $(this).data("discipline") == text;
+        }).css("font-weight","bold");
+    }
+
+    function scrolledToBottom() {
+        return $(window).scrollTop() + $(window).height() == $(document).height()
+    }
+
+    updateNavigator($('.discipline-section').first().data('discipline'))
+    $(window).bind('scroll', function() {
+        $('.discipline-section').each(function() {
+            var discipline             = $(this);
+            var distance_from_top      = $(window).scrollTop();
+            var position_of_discipline = discipline.position().top;
+
+            if (scrolledToBottom()) {
+                updateNavigator($('.discipline-section').last().data('discipline'))
+            } else if (position_of_discipline <= distance_from_top + ($(window).height() / 2)) {
+                updateNavigator(discipline.data('discipline'))
+            }
+        });
+    });
+    $('#nav-button').click(openNavigator);
+    $('#nav-section').click(openNavigator);
 });
